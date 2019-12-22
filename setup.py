2fc00313a25974ed11eee9113e9ed1f75cd68cd4
RJ722 <rahul722j@gmail.com>
2019-12-22 19:42:16 +0530

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='plox',
       version='0.1',
+      description='Python implementation of the Lox programming language',
+      keywords='compiler-design',
+      author='Rahul Jha',
+      author_email='rj722@protonmail.com',
+      url='https://github.com/RJ722/plox',
       packages=['lox'],
       entry_points={
             'console_scripts': ['plox = lox.lox:main'],
