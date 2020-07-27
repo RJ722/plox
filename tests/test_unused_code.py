762b18c53560fa4e7238722524607ae3fce9e1dd
Restyled.io <commits@restyled.io>
2020-07-27 13:17:39 +0000

    Restyled by black

--- tests/test_unused_code.py
+++ tests/test_unused_code.py
@@ -8,13 +8,13 @@ def v():
 
 
 def test_unused_code(v):
-    v.scavenge(['lox'])
+    v.scavenge(["lox"])
     v.report()
     assert v.get_unused_code() == []
 
 
 def test_unused_test(v):
-    v.scavenge(['tests'])
+    v.scavenge(["tests"])
     v.report()
     assert v.get_unused_code() == []
 
