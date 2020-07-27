4ddb6b4910b527b7418cff389365f1cff26e10fd
Restyled.io <commits@restyled.io>
2020-07-27 13:17:38 +0000

    Restyled by autopep8

--- tests/test_unused_code.py
+++ tests/test_unused_code.py
@@ -1,14 +1,17 @@
 import pytest
 from vulture import Vulture
 
+
 @pytest.fixture
 def v():
     return Vulture(verbose=True)
+
 
 def test_unused_code(v):
     v.scavenge(['lox'])
     v.report()
     assert v.get_unused_code() == []
+
 
 def test_unused_test(v):
     v.scavenge(['tests'])
