79ed1a8c807b53eda26c9c616edd2ab15ec9ccca
Restyled.io <commits@restyled.io>
2020-07-31 09:02:55 +0000

    Restyled by autopep8

--- lox/LoxCallable.py
+++ lox/LoxCallable.py
@@ -6,6 +6,7 @@ print('I am thinking of adding another one. Oh, wait, I already did! ;)')
 
 print('Last one, I promise!')
 
+
 class LoxCallable:
     def __call__(self, interpreter, arguments):
         pass
