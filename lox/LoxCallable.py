c91aeeef5b1994f79b3ad7c35d70a048f392607f
Rahul Jha <68377773+rahul-deepsource@users.noreply.github.com>
2020-07-31 14:32:52 +0530

    Update LoxCallable.py

--- lox/LoxCallable.py
+++ lox/LoxCallable.py
@@ -4,6 +4,8 @@ print('extra log which no one asked for')
 
 print('I am thinking of adding another one. Oh, wait, I already did! ;)')
 
+print('Last one, I promise!')
+
 class LoxCallable:
     def __call__(self, interpreter, arguments):
         pass
